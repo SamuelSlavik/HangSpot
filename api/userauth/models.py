e9199feaaa29d0838f41067ccb48a1450e579fc7
AdamPekny <peknyadam9@gmail.com>
2022-11-14 18:20:22 +0100

    initial commit with backend api base

---
+++ api/userauth/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = (
+        'username',
+    )
+
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return self.username
