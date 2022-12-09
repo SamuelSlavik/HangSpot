b5e6a32afd685ad5d193138913f4924aa05d3404
AdamPekny <peknyadam9@gmail.com>
2022-12-09 18:21:45 +0100

    Add endpoints swagger.

--- api/api/settings.py
+++ api/api/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -31,7 +30,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
+    'rest_framework_swagger',
 
     'userauth',
     'spots',
@@ -75,12 +74,14 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'staticfiles': 'django.templatetags.static',
+            },
         },
     },
 ]
 
 WSGI_APPLICATION = 'api.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -98,7 +99,6 @@ DATABASES = {
         }
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -120,8 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'userauth.User'
 
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -132,7 +130,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -147,5 +144,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
--- api/userauth/views.py
+++ api/userauth/views.py
@@ -7,20 +7,35 @@ from .serializers import UserSerializer
 
 # Create your views here.
 class UserListView(generics.ListAPIView):
+    """
+    Lists all users
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserCreateView(generics.CreateAPIView):
+    """
+    Creates new user
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    """
+    Use:
+    GET - to get specific user;
+    PUT - to update all user data;
+    PATCH - to update only passed data;
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDestroyView(generics.DestroyAPIView):
+    """
+    Deletes user
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
