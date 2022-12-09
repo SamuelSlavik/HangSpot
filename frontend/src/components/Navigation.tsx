3a2bd6dc5fd2b268535c3cbc60510c872b1ba986
SamuelSlavik <samuel.slavik@protonmail.com>
2022-12-09 18:26:20 +0100

    fixed backend directory

---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {NavLink, Link} from "react-router-dom";
+
+// ICONS //
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import {SvgIcon} from "@mui/material";
+
+function Navigation():JSX.Element {
+  return (
+    <div className={"navigation"}>
+      <div className={"navigation__logo"}>
+        <NavLink to={"/"} ><SvgIcon component={PushPinOutlinedIcon} fontSize={"large"}/></NavLink>
+      </div>
+      <div className={"navigation__search"}>
+        <NavLink to={"/about"} >O Wydrách</NavLink>
+      </div>
+      <div className={"navigation__links"}>
+        <NavLink to={"/createPost"} ><SvgIcon component={AddOutlinedIcon} fontSize={"large"}/></NavLink>
+        <NavLink to={"/login"} ><SvgIcon component={PersonOutlineOutlinedIcon} fontSize={"large"}/></NavLink>
+        <NavLink to={"/profile"} ><SvgIcon component={PersonOutlineOutlinedIcon} fontSize={"large"}/></NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
