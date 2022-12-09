6bdb0681e937376f42c542f525154f84b36027f9
AdamPekny <peknyadam9@gmail.com>
2022-12-09 16:31:47 +0100

    Add endpoints for working with users.

--- api/userauth/views.py
+++ api/userauth/views.py
@@ -9,3 +9,18 @@ from .serializers import UserSerializer
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDestroyView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
