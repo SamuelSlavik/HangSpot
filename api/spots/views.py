f2fdc422a70e406cc769d11de2671ca0a3685dac
AdamPekny <peknyadam9@gmail.com>
2022-12-12 02:18:09 +0100

    Add endpoints for spot CRUD and filter by type

--- api/spots/views.py
+++ api/spots/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from rest_framework import generics
+from itertools import chain
 
-from .models import SpotCommon, SkateSpot, SpotType
+from django.http.response import JsonResponse
+
+from rest_framework import generics
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+
+from .models import SpotCommon, SkateSpot, BMXSpot, WalkSpot, PicnicSpot, SunsetSpot, SpotType
 from .serializers import (
     SpotSerializer,
     SpotTypeSerializer,
@@ -22,18 +29,58 @@ class SpotListView(generics.ListAPIView):
     serializer_class = SpotSerializer
 
 
-class SpotCreateView(generics.CreateAPIView):
+@api_view(['POST'])
+def spot_create_view(request, *args, **kwargs):
     """
     Creates spot
     """
-    def get_serializer_class(self):
-        data = self.request.POST
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
         type_name = data.get('spot_type')
         try:
-            type_serializer = SpotTypeSerializer(SpotType.objects.get(type_name=type_name))
-            return type_serializer.get_spot_serializer_class()
+            type_serializer = SpotTypeSerializer(SpotType.objects.get(pk=type_name))
+            serializer_class = type_serializer.get_spot_serializer_class()
+            if serializer_class is None:
+                return JsonResponse({'Error': f'Unknown spot type {type_name}'}, status=400)
+            serializer = serializer_class(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.errors, status=400)
         except SpotType.DoesNotExist:
-            return SkateSpotSerializer
+            return JsonResponse({'Error': 'Spot type missing'}, status=400)
+
+
+# TODO enable updating spot type
+class SpotRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    def get_serializer_class(self):
+        try:
+            spot_type = SpotCommon.objects.get(pk=self.kwargs['pk']).spot_type
+            spot_type_serializer = SpotTypeSerializer(spot_type)
+            serializer_class = spot_type_serializer.get_spot_serializer_class()
+            if serializer_class is None:
+                raise NotFound(detail='Unknown spot type')
+            return serializer_class
+        except (SpotCommon.DoesNotExist, KeyError):
+            raise NotFound(detail='Spot not found')
+
+    def get_queryset(self):
+        qs = SkateSpot.objects.filter(pk=self.kwargs['pk'])
+        if qs.exists():
+            return qs
+        qs = BMXSpot.objects.filter(pk=self.kwargs['pk'])
+        if qs.exists():
+            return qs
+        qs = WalkSpot.objects.filter(pk=self.kwargs['pk'])
+        if qs.exists():
+            return qs
+        qs = PicnicSpot.objects.filter(pk=self.kwargs['pk'])
+        if qs.exists():
+            return qs
+        qs = SunsetSpot.objects.filter(pk=self.kwargs['pk'])
+        if qs.exists():
+            return qs
+        return NotFound()
 
 
 class SpotFilterTypeListView(generics.ListAPIView):
@@ -41,6 +88,8 @@ class SpotFilterTypeListView(generics.ListAPIView):
 
     def get_queryset(self):
         spot_type = self.kwargs['spot_type']
+        if spot_type == 'all':
+            return SpotCommon.objects.all()
         return SpotCommon.objects.filter(spot_type__type_name=spot_type)
 
 
